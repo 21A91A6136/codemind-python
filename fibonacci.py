718cb3b0207292e4aa1507946e64bc0152b5c30a
21A1A6136 <97614574+21A1A6136@users.noreply.github.com>
2022-05-07 10:49:34 +0530

    def fibonacci(num):     num1=0     num2=1     series=0     for i in range(num):         print(series,end=" ")         num1=num2         num2=series         series=num1+num2 num=int(input()) fibonacci(num)

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci(num):
+    num1=0
+    num2=1
+    series=0
+    for i in range(num):
+        print(series,end=" ")
+        num1=num2
+        num2=series
+        series=num1+num2
+num=int(input())
+fibonacci(num)
