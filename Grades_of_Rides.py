5610e0bc6358ac10078a0f77b5f6c4343acbafc8
21A1A6136 <97614574+21A1A6136@users.noreply.github.com>
2022-06-05 11:13:35 +0530

    h,s,v=map(int,input().split()) if(h>50 and s>60 and v>100):     print("10") elif(h>50 and s>60 and v<100):     print("9") elif(s>60 and v>100):     print("8") elif(h>50 and v>100):     print("7") elif(h>50 or s>60 or v>100):     print("6") else:     print("5")

---
+++ Grades_of_Rides.py
@@ -0,0 +1,13 @@
+h,s,v=map(int,input().split())
+if(h>50 and s>60 and v>100):
+    print("10")
+elif(h>50 and s>60 and v<100):
+    print("9")
+elif(s>60 and v>100):
+    print("8")
+elif(h>50 and v>100):
+    print("7")
+elif(h>50 or s>60 or v>100):
+    print("6")
+else:
+    print("5")
