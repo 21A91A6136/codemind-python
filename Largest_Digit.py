388985b859bc1411f81b232c19a8a8f7be93672c
21A1A6136 <97614574+21A1A6136@users.noreply.github.com>
2022-06-29 11:06:40 +0530

    a=int(input()) k=0 while(a):     d=a%10     if(k<d):         k=d     a//=10 print(k)

--- Largest_Digit.py
+++ Largest_Digit.py
@@ -1,8 +1,8 @@
-n=int(input())
-max=0
-while(n>0):
-    r=n%10
-    if max<r:
-        max=r
-    n=n//10
-print(max)
+a=int(input())
+k=0
+while(a):
+    d=a%10
+    if(k<d):
+        k=d
+    a//=10
+print(k)
