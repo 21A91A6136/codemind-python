08a578a08b3823e05a2929e172da236876dde0cc
21A1A6136 <97614574+21A1A6136@users.noreply.github.com>
2022-06-29 11:00:59 +0530

    n=int(input()) count=0 rev=0 for i in range(1,n+1):     if(n%i==0):         count+=1 if(count==2):     while(n!=0):         rem=n%10         rev=rev*10+rem         n//=10     fcount=0     for j in range(1,rev+1):         if(rev%j==0):             fcount+=1     if(fcount==2):         print("circular prime")     else:         print("prime but not a circular prime") else:     print("not prime")

--- circular_prime.py
+++ circular_prime.py
@@ -1,21 +1,21 @@
-def is_prime(s):
-    if s==1:
-        return False
-    for i in range(2,s//2):
-        if s%i==0:
-            return False
-    return True
-
-x = int(input())
-s=0
-a=x
-while x!=0:
-    v = x%10
-    s = s*10+v
-    x = x//10
-if is_prime(s) and is_prime(a):
-    print('circular prime')
-elif(is_prime(a)):
-    print('prime but not a circular prime')
+n=int(input())
+count=0
+rev=0
+for i in range(1,n+1):
+    if(n%i==0):
+        count+=1
+if(count==2):
+    while(n!=0):
+        rem=n%10
+        rev=rev*10+rem
+        n//=10
+    fcount=0
+    for j in range(1,rev+1):
+        if(rev%j==0):
+            fcount+=1
+    if(fcount==2):
+        print("circular prime")
+    else:
+        print("prime but not a circular prime")
 else:
-    print('not prime')
+    print("not prime")
