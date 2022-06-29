e3ffb1f908d70c8b84d3ba4299b6226afb75d92d
21A1A6136 <97614574+21A1A6136@users.noreply.github.com>
2022-06-29 10:52:07 +0530

    n=int(input()) a,b=0,1 while(True):     c=a+b     if(c>n):        h1=b        h2=c        break     a=b     b=c #print(h1,h2) x1=abs(h1-n) x2=abs(h2-n) if(x1==x2):    print(h1,h2) elif(x1>x2):    print(h2) else:    print(h1)

--- nearest_fibonacci.py
+++ nearest_fibonacci.py
@@ -1,17 +1,19 @@
 n=int(input())
-a=0
-b=1
-while(a<=n):
-    i=a
+a,b=0,1
+while(True):
     c=a+b
+    if(c>n):
+       h1=b
+       h2=c
+       break
     a=b
     b=c
-    s=a
-x=abs(n-i)
-y=abs(n-s)
-if(x<y):
-   print(i)
-elif(y==x):
-   print(i,s)
+#print(h1,h2)
+x1=abs(h1-n)
+x2=abs(h2-n)
+if(x1==x2):
+   print(h1,h2)
+elif(x1>x2):
+   print(h2)
 else:
-   print(s)
+   print(h1)
