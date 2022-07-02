07b144e50c64f6800ad8adeb0103df1dffbeb7eb
21A1A6136 <97614574+21A1A6136@users.noreply.github.com>
2022-07-02 19:48:50 +0530

    def prime(n):     c=0     for i in range(1,n+1):         if(n%i==0):             c+=1     if(c==2):         return 1     else:         return 0 h=int(input()) while(h):     n=int(input())     for i in range(1,n+1):         if(prime(i)):             a=i     i=n+1     while(1):         if(prime(i)):             b=i             break         i+=1     x=abs(a-n)     y=abs(b-n)     if(x<y):         print(a)     elif(x==y):         print(a)     else:         print(b)     h-=1

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,28 +1,30 @@
-def prime(j):
-    i=2
-    v=0
-    while i!=j:
-        if j%i==0:
-            v=1
-        i+=1
-    if v==0:
-        return j
-x = int(input())
-for i in range(x):
-    y = int(input())
-    b=y
-    for j in range(y,1,-1):
-        if prime(j):
-            n=j
-            break
-    for k in range(2,y):
-        if prime(b):
-            m=b
-            break
-        b+=1
-    if (y-n)<(m-y):
-        print(n)
-    elif (y-n)==(m-y):
-        print(n)
+def prime(n):
+    c=0
+    for i in range(1,n+1):
+        if(n%i==0):
+            c+=1
+    if(c==2):
+        return 1
     else:
-        print(m)
+        return 0
+h=int(input())
+while(h):
+    n=int(input())
+    for i in range(1,n+1):
+        if(prime(i)):
+            a=i
+    i=n+1
+    while(1):
+        if(prime(i)):
+            b=i
+            break
+        i+=1
+    x=abs(a-n)
+    y=abs(b-n)
+    if(x<y):
+        print(a)
+    elif(x==y):
+        print(a)
+    else:
+        print(b)
+    h-=1
